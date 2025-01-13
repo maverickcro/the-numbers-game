b63eee389feece476fccd833134d39f4e9158183
Marko <mav3ricktv@gmail.com>
2025-01-13 21:39:01 +0100

    React Helmet and favicon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
 import Navigation from "./components/Navigation";
 import MainScreen from "./components/MainScreen";
 import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
+import newLogo from "./components/assets/newLogo.png";
 
 const App = () => {
   const defaultSettings = {
@@ -26,6 +28,33 @@ const App = () => {
 
   return (
     <div className="App">
+      <Helmet>
+        <title>That Numbers Game - Test Your Skills and Luck!</title>
+        <meta
+          name="description"
+          content="Play That Numbers Game, a fun and challenging game where you place random numbers in ascending order. Test your skills and luck now!"
+        />
+        <meta
+          property="og:title"
+          content="That Numbers Game - Test Your Skills and Luck!"
+        />
+        <meta
+          property="og:description"
+          content="Play That Numbers Game, a fun and challenging game where you place random numbers in ascending order. Test your skills and luck now!"
+        />
+        <meta property="og:image" content={newLogo} />
+        <meta property="og:url" content="https://thatnumbersgame.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="That Numbers Game - Test Your Skills and Luck!"
+        />
+        <meta
+          name="twitter:description"
+          content="Play That Numbers Game, a fun and challenging game where you place random numbers in ascending order. Test your skills and luck now!"
+        />
+        <meta name="twitter:image" content={newLogo} />
+      </Helmet>
       <Navigation updateSettings={updateSettings} />
       <MainScreen settings={settings} />
       <Analytics />
